7bab8399c0b9c988920e1881fc730d5034fb5826
zhourongchang399 <497447111@qq.com>
2025-02-13 15:00:53 +0800

    WeMediaAndWeMediaGatewayCommit

---
+++ heima-leadnews-gateway/heima-leadnews-media-gateway/src/main/java/com/heima/MediaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.heima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaGatewayApplication.class, args);
+    }
+
+}
---
+++ heima-leadnews-gateway/heima-leadnews-media-gateway/src/test/java/com/heima/HeimaLeadnewsMediaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimaLeadnewsMediaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
