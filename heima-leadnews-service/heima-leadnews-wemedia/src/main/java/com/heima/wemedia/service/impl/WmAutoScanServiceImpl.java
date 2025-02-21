e1f2fa197f85268c364206002b24278d6dc31b90
zhourongchang399 <497447111@qq.com>
2025-02-21 20:51:04 +0800

    OCRscanModuleCommit

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmAutoScanServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmAutoScanServiceImpl.java
@@ -10,16 +10,24 @@ import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.wemedia.pojos.WmChannel;
 import com.heima.model.wemedia.pojos.WmNews;
 import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.utils.common.OCRUtil;
 import com.heima.wemedia.mapper.WmChannelMapper;
 import com.heima.wemedia.mapper.WmNewsMapper;
 import com.heima.wemedia.mapper.WmUserMapper;
 import com.heima.wemedia.service.WmAutoScanService;
 import com.heima.wemedia.service.WmSensitiveService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,6 +41,7 @@ import java.util.stream.Stream;
 @Async
 public class WmAutoScanServiceImpl implements WmAutoScanService {
 
+    private static final Logger log = LoggerFactory.getLogger(WmAutoScanServiceImpl.class);
     @Autowired
     private WmNewsMapper wmNewsMapper;
 
@@ -67,16 +76,20 @@ public class WmAutoScanServiceImpl implements WmAutoScanService {
         Short status = WemediaConstants.WM_NEWS_PASS_CHECK;
         String reason = WemediaConstants.CHECK_SUCCEED;
 
-        // 文章文本内容审核
+        // 文章审核
+        // 获取文字文本内容
         StringJoiner stringJoiner = new StringJoiner(",");
-        textAndImages.get(0).forEach(stringJoiner::add);
+        textAndImages.get(0).stream().forEach(stringJoiner::add);
+        // 通过OCR获取图片内文字
+        List<String> scannedText = ocrImages(textAndImages.get(1));
+        scannedText.stream().forEach(stringJoiner::add);
+
+        // 文章文字审核
         Map<String, Integer> resultMap = wmSensitiveService.textCheck(stringJoiner.toString());
         if (!resultMap.isEmpty()) {
             status = WemediaConstants.WM_NEWS_FAIL_CHECK;
             reason = WemediaConstants.CHECK_FAIL_SENSITIVE + resultMap.toString();
         }
-
-        // TODO 文章图片内容审核
 
         // 调用APP端保存审核通过的文章
         Long articleId = null;
@@ -87,6 +100,40 @@ public class WmAutoScanServiceImpl implements WmAutoScanService {
         // 更新文章审核信息
         updateNewsCheckInfo(id, status, reason, articleId);
 
+    }
+
+    private List<String> ocrImages(List<String> imagesList) {
+        List<String> imagesText = new ArrayList<>();
+        for (String imageUrl : imagesList) {
+            log.info("url:{}", imageUrl);
+            try {
+                URL url = new URL(imageUrl);
+                URLConnection urlConnection = url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                byte[] data = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, bytesRead);
+                }
+                buffer.flush();
+                byte[] imageBytes = buffer.toByteArray();
+
+                BufferedImage imageFile = ImageIO.read(new ByteArrayInputStream(imageBytes));
+
+                if (imageFile != null) {
+                    String scannedText = OCRUtil.scanImage(imageFile);
+                    imagesText.add(scannedText);
+                } else {
+                    System.err.println("Failed to read the image.");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return imagesText;
     }
 
     private void updateNewsCheckInfo(Integer id, Short status, String reason, Long articleId) {
---
+++ heima-leadnews-utils/src/main/java/com/heima/utils/common/OCRUtil.java
@@ -0,0 +1,58 @@
+package com.heima.utils.common;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import org.apache.xmlgraphics.image.loader.impl.ImageBuffered;
+import org.checkerframework.checker.index.qual.SameLen;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * @author ：Zc
+ * @description：TODO
+ * @date ：2025/2/21 16:34
+ */
+@Component
+@Data
+@ConfigurationProperties(prefix = "tezz4j.ocr")
+@Slf4j
+public class OCRUtil {
+
+    static String path = "D:\\Code\\heima-leadnews\\heima-leadnews-utils\\src\\main\\resources\\tessdata";
+    static String language = "chi_sim";
+
+    public static String scanImage(BufferedImage bufferedImage){
+        String result = null;
+        try {
+            //创建Tesseract对象
+            ITesseract tesseract = new Tesseract();
+            //设置字体库路径
+            tesseract.setDatapath(path);
+            //中文识别
+            tesseract.setLanguage(language);
+            log.info("path:{}", path);
+            log.info("language:{}", language);
+            //执行ocr识别
+            result = tesseract.doOCR(bufferedImage);
+            //替换回车和tal键  使结果为一行
+            result = result.replaceAll("\\r|\\n", "-").replaceAll(" ", "");
+            System.out.println("识别的结果为：" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        File file = new File("C:\\Users\\49744\\Desktop\\textOCR.jpg");
+//        path = "D:\\Code\\heima-leadnews\\heima-leadnews-utils\\src\\main\\resources\\tessdata";
+//        language = "chi_sim";
+//        scanImage(file);
+//    }
+
+}
